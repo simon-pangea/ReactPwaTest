d594ab874a64774717de090d1db0a554f99dc0f0
simon polishuk <simonp@pangea-it.com>
2022-02-17 16:36:59 +0200

    for jenkins

---
+++ Jenkinsfile
@@ -0,0 +1,4 @@
+#!groovy //Language declaration
+@Library('Pangea.Jenkins.SharedLibrary@master') _  //Reference to the shared library where the template exists
+
+genericReactSiteOnIIS()  //Executing the relevant pipeline from Pangea Jenkins shared library
--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -1,35 +1,32 @@
-import React, { useRef, useState } from "react";
+import React, { Component } from "react";
 
-export default function Notification() {
-  const notifyMe = () => {
-    // Let's check if the browser supports notifications
+class SimpleNotification extends Component {
+  constructor() {
+    super();
+    this.showNotification = this.showNotification.bind(this);
+  }
+
+  componentDidMount() {
     if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification");
+      console.log("This browser does not support desktop notification");
+    } else {
+      Notification.requestPermission();
     }
+  }
 
-    // Let's check whether notification permissions have already been granted
-    else if (Notification.permission === "granted") {
-      // If it's okay let's create a notification
-      var notification = new Notification("Hi there!");
-    }
+  showNotification() {
+    new Notification('Hey')
+  }
 
-    // Otherwise, we need to ask the user for permission
-    else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then(function (permission) {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-          var notification = new Notification("Hi there!");
-        }
-      });
-    }
-
-    // At last, if the user has denied notifications, and you
-    // want to be respectful there is no need to bother them any more.
-  };
-
-  return (
-    <div>
-      <button onClick={notifyMe}>NOTIFICATION</button>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <button onClick={this.showNotification}>
+          SHOW NOTIFICATION
+        </button>
+      </div>
+    );
+  }
 }
+
+export default SimpleNotification;
